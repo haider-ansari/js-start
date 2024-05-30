2f1767c08fa94f2b35099d3f2bfa173d011e78e8
Haider Ansari <md.haideransari120@gmail.com>
2024-05-30 12:58:00 +0530

    opertations

--- 01-Basics/03-conversionOperation.js
+++ 01-Basics/03-conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "hitesh";
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -16,7 +16,7 @@ console.log(valueInNumber);
 let isLoggedIn = "haider";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -26,7 +26,25 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+//********* Operations *********
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2**3);
+
+let str1 = "hello";
+let str2 = "haider";
+
+let str3 = (str1 + str2);
+// console.log(str3);
+
+
 
 
