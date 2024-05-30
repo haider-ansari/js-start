6ad4aca626b8388f666fb3bfca53882a78c126d2
Haider Ansari <md.haideransari120@gmail.com>
2024-05-30 11:47:34 +0530

    datatypes

--- 01-Basics/02-datatypes.js
+++ 01-Basics/02-datatypes.js
@@ -1,1 +1,20 @@
 "use strict" // treat all javascript code as newer version
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+//Datatypes:
+//-----------------------
+//number => 2 to power 53
+//bigint
+//boolean => true/false
+//null => standalone value, it is an object
+//symbol => when we want uniqueness in our code
+
+
+//object 
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
